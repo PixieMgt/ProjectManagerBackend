af101b55042682456d3923df056063043704bea4
Pixie Moortgat <pixiemoortgat@outlook.com>
2026-03-18 14:29:24 +0100

    fixed get all activities route definition

--- src/routes/activities.routes.js
+++ src/routes/activities.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/activities", (req, res) => {
+router.get("/", (req, res) => {
   res.status(200).json({ message: "under construction" });
 });
 
